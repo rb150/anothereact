faa9c74c298c618f32ff7b23ba5fe28a11cb4f4b
rb150 <rbajaj852@gmail.com>
2019-03-22 19:22:13 -0500

    real right way to duplicate syntax

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ class App extends Component {
     // );
 
     //return React.createElement('div', null, 'h1', 'Hi, I\'m a React App!!!');
-    return React.createElement('div', null, React.createElement('h1', null, 'Does this work now?'))
+    
+    //add styling by replacing null with an object that has our styling.
+    //this (below) is what the above code will get compiled into
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'))
   }  
 }
 
