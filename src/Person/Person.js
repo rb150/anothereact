411cf1b529e55a6cebe8bf13753f2ac60a9035e0
Work Horse <rbajaj852@gmail.com>
2019-03-23 14:02:02 -0500

    Added Person component

---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react' //must always import react 
+
+
+//component is function that returns jsx.  Written like any other function,
+//as long as it returns jsx.  If using jsx, must import react
+const person = () => {
+    return <p>I'm a person</p>
+}
+
+export default person; //export function as default of the file in person constant which holds
+//the function
