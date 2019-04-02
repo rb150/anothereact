812fad26e141815ce2a7b3e577b244c65e6f023a
Work Horse <rbajaj852@gmail.com>
2019-04-01 19:10:57 -0500

    pushed info from persons array in state object

--- src/App.js
+++ src/App.js
@@ -5,20 +5,31 @@ should have uppercase.  In jsx, all elements starting with lower case elements
 are reserved for native html elements (<Div> (custom) vs <div>)
 */
 import Person from './Person/Person' 
+//NOTE: classes have properties which are like variables of a class (dont use var)
 
+//state is managed from inside the component as opposed to props with is outside the component
 class App extends Component {
+  state = { //reserved word in jsx
+    persons: [ //array of objects
+      {name: 'Max', age: 28},
+      {name: 'Manu', age:29},
+      {name: 'Stephanie', age: 26}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
+      <button>Switch Name</button>
        {/*Able to use self closing tag for Person since we
         don't nest anything in between the element*/}
         {/*variables/attributes (name and age) are accessed
         by using props in the component file*/}
-      <Person name="John" age="20"/> 
-      <Person name="Jim" age="21"/> 
-      <Person name="Jill" age="22"/> 
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/> 
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/> 
       </div>
     );
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,10 +6,20 @@ import React from 'react' //must always import react
 
 
 const person = (props) => { //react uses props object to access attributes
+    //curly braces are used to make an element dynamic
     //must use the name of the attributes (in the file that imports this file
     //all attributes are under the props object
-    return <p>I'm a person and I am {props.name} and {props.age} years old</p>
-}
+    return ( //outputting multiple elements, hence the parenthesis
+    <div> {/*jsx components must have only one parent element.  Hence the multiple
+    p elements must be contained in one div element*/}
+    <p>I'm {props.name} and {props.age} years old</p>
+{/*props.children is reserved.  Nothing was passed but this reserved
+keyword will take whatever is pass between the opening and closing tag
+of the element in the main component*/}
+    <p>{props.children}</p>
+    </div>
+    )
+};
 
 export default person; //export function as default of the file in person constant which holds
 //the function
